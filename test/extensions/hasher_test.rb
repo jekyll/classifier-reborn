defda7ddaa35945fef0794030f4f7c274ccf86b4
Alexander Adam <mail@alexander-adam.info>
2015-11-01 00:26:11 +0100

    update .travis.yml & fix older ruby compatibility

--- test/extensions/hasher_test.rb
+++ test/extensions/hasher_test.rb
@@ -6,7 +6,7 @@ class HasherTest < Test::Unit::TestCase
   end
 
   def test_word_hash
-    hash = { good: 1, "!": 1, hope: 1, "'": 1, ".": 1, love: 1, word: 1, them: 1, test: 1 }
+    hash = { good: 1, :'!' => 1, hope: 1, :"'" => 1, :'.' => 1, love: 1, word: 1, them: 1, test: 1 }
     assert_equal hash, Hasher.word_hash("here are some good words of test's. I hope you love them!")
   end
 
