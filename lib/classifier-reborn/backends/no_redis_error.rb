e1d6060b651f270c226b42ff693e0509a25129ee
Chase Gilliam <chase.gilliam@gmail.com>
2017-01-07 19:05:00 -0500

    Update no_redis_error.rb

--- lib/classifier-reborn/backends/no_redis_error.rb
+++ lib/classifier-reborn/backends/no_redis_error.rb
@@ -4,7 +4,7 @@ class NoRedisError < LoadError
       %q{The Redis Backend can only be used if Redis is installed.
         This error is raised from 'lib/classifier-reborn/backends/bayes_redis_backend.rb'.
         If you have encountered this error and would like to use the Redis Backend,
-        please run 'gem install redis' or include 'gem "redis", "~>3.2"' in
+        please run 'gem install redis' or include 'gem "redis"' in
         your gemfile. For more info see https://github.com/jekyll/classifier-reborn#usage.
       }
     super(msg)
