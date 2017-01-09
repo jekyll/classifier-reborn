23e6bf4e8ec6800ef7a5116220d71cc5bab186df
Sawood Alam <ibnesayeed@gmail.com>
2017-01-09 01:48:52 -0500

    Benchmark refactoring, improving efficiency, enhanced reporting

--- test/bayes/bayesian_common_benchmarks.rb
+++ test/bayes/bayesian_common_benchmarks.rb
@@ -1,9 +1,35 @@
 # encoding: utf-8
 
 module BayesianCommonBenchmarks
-  def load_data
+  MAX_RECORDS = 5000
+
+  class BenchmarkReporter < Minitest::Reporters::RubyMateReporter
+    def before_suite(suite)
+      puts ([suite] + BayesianCommonBenchmarks.bench_range).join("\t")
+    end
+    def after_suite(suite)
+      puts
+    end
+  end
+  Minitest::Reporters.use! BenchmarkReporter.new
+
+  def self.bench_range
+    (Minitest::Benchmark.bench_exp(1, MAX_RECORDS) << MAX_RECORDS).uniq
+  end
+
+  def self.data
+    @@data ||= self.load_data
+  end
+
+  def self.load_data
     sms_spam_collection = File.expand_path(File.dirname(__FILE__) + '/../data/corpus/SMSSpamCollection.tsv')
-    File.read(sms_spam_collection).force_encoding("utf-8").split("\n")
+    lines = File.read(sms_spam_collection).force_encoding("utf-8").split("\n")
+    puts "\n\e[31mInsufficient records in the dataset: available #{lines.length}, required #{MAX_RECORDS}\e[0m\n\n" if lines.length < MAX_RECORDS
+    lines
+  end
+
+  def self.insufficient_data?
+    self.data.length < MAX_RECORDS
   end
 
   def bench_train
--- test/bayes/bayesian_redis_benchmark.rb
+++ test/bayes/bayesian_redis_benchmark.rb
@@ -4,19 +4,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 require_relative './bayesian_common_benchmarks'
 
 class BayesianRedisBenchmark < Minitest::Benchmark
-  MAX_RECORDS = 5000
-
   include BayesianCommonBenchmarks
 
   def self.bench_range
-    (bench_exp(1, MAX_RECORDS) << MAX_RECORDS).uniq
+    BayesianCommonBenchmarks.bench_range
   end
 
   def setup
-    @data ||= load_data
-    if @data.length < MAX_RECORDS
-      skip("Not enough records in the dataset")
-    end
+    skip if BayesianCommonBenchmarks.insufficient_data?
+    @data = BayesianCommonBenchmarks.data
     @classifiers = {}
     self.class.bench_range.each_with_index do |n, i|
       begin
@@ -27,12 +23,13 @@ class BayesianRedisBenchmark < Minitest::Benchmark
         skip(e)
       end
     end
-    print "redis_"
   end
 
   def teardown
-    self.class.bench_range.each do |n|
-      @classifiers[n].instance_variable_get(:@backend).instance_variable_get(:@redis).flushdb
+    if defined? @classifiers
+      self.class.bench_range.each do |n|
+        @classifiers[n].instance_variable_get(:@backend).instance_variable_get(:@redis).flushdb
+      end
     end
   end
 end
