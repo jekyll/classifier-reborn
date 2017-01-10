a18093b651d17f2f60ab5ffca5ec704aeb515a0c
Sawood Alam <ibnesayeed@gmail.com>
2017-01-09 20:21:51 -0500

    Removed unnecessary white space

--- test/data/test_data_loader.rb
+++ test/data/test_data_loader.rb
@@ -9,7 +9,7 @@ module TestDataLoader
     sms_spam_collection = File.expand_path(File.dirname(__FILE__) + '/corpus/SMSSpamCollection.tsv')
     File.read(sms_spam_collection).force_encoding("utf-8").split("\n")
   end
-  
+
   def self.report_insufficient_data(available, required)
     puts "\e[31mInsufficient records in the dataset: available #{available}, required #{required}\e[0m"
   end
