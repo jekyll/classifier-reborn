1b26dac15d4f142dc12b49a4cd6f7db69b6d5d72
Sawood Alam <ibnesayeed@gmail.com>
2017-01-20 21:40:43 -0500

    Reporting confusion matrix with various derived stats

--- lib/classifier-reborn/validators/classifier_validator.rb
+++ lib/classifier-reborn/validators/classifier_validator.rb
@@ -63,6 +63,8 @@ module ClassifierReborn
       puts
       print_conf_mat(accumulated_conf_mat)
       puts
+      conf_tab = conf_mat_to_tab(accumulated_conf_mat)
+      print_conf_tab(conf_tab)
     end
 
     def calculate_stats(conf_mat)
@@ -74,11 +76,22 @@ module ClassifierReborn
           else
             incorrect += v
           end
-        end  
+        end
       end
       total = correct + incorrect
-      accuracy = total.zero? ? 0 : correct / total.to_f
-      {total: total, correct: correct, incorrect: incorrect, accuracy: accuracy}
+      {total: total, correct: correct, incorrect: incorrect, accuracy: divide(correct, total)}
+    end
+
+    def conf_mat_to_tab(conf_mat)
+      conf_tab = Hash.new {|h, k| h[k] = {p: {t: 0, f: 0}, n: {t: 0, f: 0}}}
+      conf_mat.each_key do |positive|
+        conf_mat.each do |actual, cols|
+          cols.each do |predicted, v|
+            conf_tab[positive][positive == predicted ? :p : :n][actual == predicted ? :t : :f] += v
+          end
+        end
+      end
+      conf_tab
     end
 
     def print_stats(stats, prefix="")
@@ -103,8 +116,49 @@ module ClassifierReborn
       puts (["Total".ljust(cell_size)] + predicted_totals.values.map{|v| v.to_s.rjust(cell_size)} + [predicted_totals.values.reduce(:+).to_s.rjust(cell_size)]).join(" ")
     end
 
+    def print_conf_tab(conf_tab)
+      conf_tab.each do |positive, tab|
+        puts "# Positive class: #{positive}"
+        derivations = conf_tab_derivations(tab)
+        print_derivations(derivations)
+        puts
+      end
+    end
+
+    def conf_tab_derivations(tab)
+      positives = tab[:p][:t] + tab[:n][:f]
+      negatives = tab[:n][:t] + tab[:p][:f]
+      total     = positives + negatives
+      {
+        total_population:   positives + negatives,
+        condition_positive: positives,
+        condition_negative: negatives,
+        prevalence:         divide(positives, total),
+        true_positive:      tab[:p][:t],
+        true_negative:      tab[:n][:t],
+        false_positive:     tab[:p][:f],
+        false_negative:     tab[:n][:f],
+        specificity:        divide(tab[:n][:t], negatives),
+        recall:             divide(tab[:p][:t], positives),
+        precision:          divide(tab[:p][:t], tab[:p][:t] + tab[:p][:f]),
+        accuracy:           divide(tab[:p][:t] + tab[:n][:t], total),
+        f1_score:           divide(2 * tab[:p][:t], 2 * tab[:p][:t] + tab[:p][:f] + tab[:n][:f])
+      }
+    end
+
+    def print_derivations(derivations)
+      max_len = derivations.keys.map(&:length).max
+      derivations.each do |k, v|
+        puts k.to_s.tr('_', ' ').capitalize.ljust(max_len) + " : " + v.to_s
+      end
+    end
+
     def empty_conf_mat(categories)
       categories.map{|actual| [actual, categories.map{|predicted| [predicted, 0]}.to_h]}.to_h
+    end
+
+    def divide(dividend, divisor)
+      divisor.zero? ? 0.0 : dividend / divisor.to_f
     end
   end
 end
