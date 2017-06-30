c6c88a59563dfcf87e04ac6a640cb6dbf7920685
YUKI Hiroshi <shimoda@clear-code.com>
2017-06-30 21:17:14 +0900

    Revert "Fix indent"
    This reverts commit 07e68073067cb9f564122683c73cbbb324e5d4f5.
    Rollback.

--- lib/classifier-reborn/extensions/tokenizer/whitespace.rb
+++ lib/classifier-reborn/extensions/tokenizer/whitespace.rb
@@ -15,10 +15,10 @@ module ClassifierReborn
           Token.new(word, stemmable: true, maybe_stopword: true)
         end
         unless clean
-          symbol_tokens = str.scan(/[^\s\p{WORD}]/).collect do |word|
-            Token.new(word, stemmable: false, maybe_stopword: false)
-          end
-          tokens += symbol_tokens
+        symbol_tokens = str.scan(/[^\s\p{WORD}]/).collect do |word|
+          Token.new(word, stemmable: false, maybe_stopword: false)
+        end
+        tokens += symbol_tokens
         end
         tokens = TokenFilter::Stopword.filter(tokens, language: language)
         if enable_stemmer
