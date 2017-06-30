d7b25194aedef28e4885091fcc9115b77b0a8c09
YUKI Hiroshi <shimoda@clear-code.com>
2017-06-30 21:18:15 +0900

    Use meaningful variable name

--- lib/classifier-reborn/extensions/tokenizer/whitespace.rb
+++ lib/classifier-reborn/extensions/tokenizer/whitespace.rb
@@ -11,16 +11,16 @@ module ClassifierReborn
       module_function
 
       def tokenize(str, clean: false)
-        word_tokens = str.gsub(/[^\p{WORD}\s]/, '').downcase.split.collect do |word|
+        tokens = str.gsub(/[^\p{WORD}\s]/, '').downcase.split.collect do |word|
           Token.new(word, stemmable: true, maybe_stopword: true)
         end
         unless clean
           symbol_tokens = str.scan(/[^\s\p{WORD}]/).collect do |word|
             Token.new(word, stemmable: false, maybe_stopword: false)
           end
-          word_tokens += symbol_tokens
+          tokens += symbol_tokens
         end
-        word_tokens
+        tokens
       end
     end
   end
