d84caa913b90d78a072a9bc44bcdbd1e0678bd19
YUKI Hiroshi <shimoda@clear-code.com>
2017-06-30 21:14:08 +0900

    Unite stemmer and stopword filter to whitespace tokenizer

--- lib/classifier-reborn/extensions/tokenizer/whitespace.rb
+++ lib/classifier-reborn/extensions/tokenizer/whitespace.rb
@@ -10,17 +10,21 @@ module ClassifierReborn
     module Whitespace
       module_function
 
-      def tokenize(str, clean: false)
-        word_tokens = str.gsub(/[^\p{WORD}\s]/, '').downcase.split.collect do |word|
+      def tokenize(str, language: 'en', enable_stemmer: true, clean: false)
+        tokens = str.gsub(/[^\p{WORD}\s]/, '').downcase.split.collect do |word|
           Token.new(word, stemmable: true, maybe_stopword: true)
         end
         unless clean
         symbol_tokens = str.scan(/[^\s\p{WORD}]/).collect do |word|
           Token.new(word, stemmable: false, maybe_stopword: false)
         end
-        word_tokens += symbol_tokens
+        tokens += symbol_tokens
         end
-        word_tokens
+        tokens = TokenFilter::Stopword.filter(tokens, language: language)
+        if enable_stemmer
+          tokens = TokenFilter::Stemmer.filter(tokens, language: language)
+        end
+        tokens
       end
     end
   end
