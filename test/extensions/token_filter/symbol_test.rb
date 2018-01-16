35c304e7ade0ee76b3e1871699d3974ddc5d1cd6
YUKI Hiroshi <shimoda@clear-code.com>
2018-01-16 16:06:00 +0900

    Add test for TokenFilter::Symbol

---
+++ test/extensions/token_filter/symbol_test.rb
@@ -0,0 +1,12 @@
+require_relative '../../test_helper'
+require 'tempfile'
+
+class TokenFilterSymbolTest < Minitest::Test
+  def test_symbol
+    words = %w(term ! ?).collect do |word|
+      Tokenizer::Token.new(word)
+    end
+    words = TokenFilter::Symbol.filter(words)
+    assert_equal %w(term), words
+  end
+end
