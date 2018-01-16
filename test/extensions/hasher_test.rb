14af4d049e49aa0e1defc8b48a9d64fde1f7da5d
YUKI Hiroshi <shimoda@clear-code.com>
2018-01-16 16:32:50 +0900

    Add tests to custom tokenizer and token filters

--- test/extensions/hasher_test.rb
+++ test/extensions/hasher_test.rb
@@ -11,6 +11,34 @@ class HasherTest < Minitest::Test
     assert_equal hash, Hasher.word_hash("here are some good words of test's. I hope you love them!")
   end
 
+  module BigramTokenizer
+    module_function
+    def tokenize(str)
+      str.each_char
+         .each_cons(2)
+         .map do |chars| ::ClassifierReborn::Tokenizer::Token.new(chars.join) end
+    end
+  end
+
+  def test_custom_tokenizer
+    hash = { te: 1, er: 1, rm: 1 }
+    assert_equal hash, Hasher.word_hash("term", tokenizer: BigramTokenizer, token_filters: [])
+  end
+
+  module CatFilter
+    module_function
+    def filter(tokens)
+      tokens.reject do |token|
+        /\Acat\z/i === token
+      end
+    end
+  end
+
+  def test_custom_token_filters
+    hash = { dog: 1 }
+    assert_equal hash, Hasher.word_hash("cat dog", token_filters: [CatFilter])
+  end
+
   def teardown
     TokenFilter::Stopword::STOPWORDS.clear
     TokenFilter::Stopword::STOPWORDS_PATH.clear.concat @original_stopwords_path
