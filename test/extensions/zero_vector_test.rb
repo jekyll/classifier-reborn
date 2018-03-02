88d7b9e224def963876b6a5536b1f512e4f153d0
sueka <mail@sueka.me>
2018-03-03 01:39:48 +0900

    add a test for zero vectors

---
+++ test/extensions/zero_vector_test.rb
@@ -0,0 +1,10 @@
+class ZeroVectorTest < Minitest::Test
+  def test_zero?
+    vec0 = Vector[]
+    vec1 = Vector[0]
+    vec10 = Vector.elements [0] * 10
+    assert vec0.zero?
+    assert vec1.zero?
+    assert vec10.zero?
+  end
+end
